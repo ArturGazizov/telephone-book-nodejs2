3662d4078f7eb2381043a07fe4be40b08a3af31a
ArturGazizov <znatokly@gmail.com>
2020-06-07 03:10:25 +0300

    Update persons.js

--- models/persons.js
+++ models/persons.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // Replace with the URL of your own database. Do not store the password on GitLab!
-const url = 'mongodb+srv://missismama:U7y6u7Y6@cluster0-wcm2d.gcp.mongodb.net/test?retryWrites=true&w=majority'
+const url = 'mongodb+srv://missismama:missismissis@cluster0-wcm2d.gcp.mongodb.net/test?retryWrites=true&w=majority'
 
 mongoose.connect(url)
 
